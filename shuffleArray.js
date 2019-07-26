4348bf37fa0eef1c0fcd6c73bf9b64ff17342e7e
estgvz <50722528+estgvz@users.noreply.github.com>
2019-07-25 18:10:31 -0700

    Update to 1.05
    1.05 removes the possibility of pulling duplicate words from the same wordlist within a single premise

---
+++ shuffleArray.js
@@ -0,0 +1,7 @@
+function shuffleArray(arr) {
+  var len = arr.length;
+  var arr2 = arr.splice(0);
+  for(var i=0;i<len;i++) {
+    arr.push(arr2.splice(Math.floor(Math.random()*arr2.length),1)[0]);
+  }
+}
