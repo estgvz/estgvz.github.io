c6d52bb65c3756a99e11260bcb02586b0fafd078
estgvz <50722528+estgvz@users.noreply.github.com>
2019-07-25 23:38:05 -0700

    Upgrade to final v1.05, bugfix
    fixed a rare word-mashing bug by improving the way words are chosen for each generated premise. also added some new words

--- shuffleArray.js
+++ shuffleArray.js
@@ -1,7 +1,8 @@
 function shuffleArray(arr) {
-  var len = arr.length;
-  var arr2 = arr.splice(0);
-  for(var i=0;i<len;i++) {
-    arr.push(arr2.splice(Math.floor(Math.random()*arr2.length),1)[0]);
-  }
+	var len = arr.length;
+	var arr2 = arr.splice(0);
+	for(var i=0;i<len;i++) {
+		arr.push(arr2.splice(Math.floor(Math.random()*arr2.length),1)[0]);
+	}
+	return 0;
 }
