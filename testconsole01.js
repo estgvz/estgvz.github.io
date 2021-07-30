f6dab1ffd3c66b6f8b1a619e8da99c15a90eae3d
estgvz <50722528+estgvz@users.noreply.github.com>
2021-07-29 22:24:46 -0700

    more developed but still nonworking web console husk

---
+++ testconsole01.js
@@ -0,0 +1,2 @@
+outText += "\n\nThis text added by this page's specific JS file";
+document.getElementById("consoleOut").innerHTML = outText;
---
+++ webconsole.js
@@ -0,0 +1,4 @@
+// web console
+
+var outText = "This text set by JS";
+document.getElementById("consoleOut").innerHTML = outText;
