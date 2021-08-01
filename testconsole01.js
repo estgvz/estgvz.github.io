5845bcab137f713330986e2be1db34feed1a2902
estgvz <50722528+estgvz@users.noreply.github.com>
2021-07-31 20:25:37 -0700

    more console husk work

--- testconsole01.js
+++ testconsole01.js
@@ -1,2 +1,2 @@
-outText += "\n\nThis text added by this page's specific JS file";
+outText += "\n\nThis text added by this page's specific JS file and should appear below it";
 document.getElementById("consoleOut").innerHTML = outText;
--- webconsole.js
+++ webconsole.js
@@ -1,4 +1,15 @@
 // web console
 
-var outText = "This text set by JS";
+var cin = "";
+var outText = "This text set by console's common JS (webconsole.js)";
 document.getElementById("consoleOut").innerHTML = outText;
+
+function toConsole() {
+	cin = document.getElementById("consoleIn").value.toString();
+	if(cin == "" || cin == undefined || cin.length < 1 || cin == " ") {
+		document.getElementById("consoleOut").innerHTML = "Bad input.";
+		return;
+	}
+	document.getElementById("consoleIn").value = "";
+	document.getElementById("consoleOut").innerHTML = "You said: "+ cin + "\n\nDummy function. Console not actually programmed.";
+}
